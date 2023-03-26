a51b6a771b00da7c73923ee656373e00a4f92034
prince954 <akprince0268@gmail.com>
2023-03-27 03:19:43 +0600

    React: Daynamic Data Component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,31 @@ import './App.css';
 
 function App() {
 
+  const naykos = ["Anawr", "Salmal Sha","alomgir"]
+
   return ( < div className = 'App' >
-    <header className = 'app-header' >
-    <p > I am a react Parson < /p> </header>
-    <Parson></Parson>
-    < Parson1></Parson1>
-    <Parson></Parson>
+    < header className = 'app-header' >
+       < p > I am a react Parson < /p> 
+     </header >
+     <Parson name='Rubel Kahan' nayka ='Moushome'> < /Parson>
+     < Parson name = {naykos[1]} nayka = 'shabana' > < /Parson> 
+     < Parson name = {naykos[2]}nayka = 'popi' > < /Parson> 
+     < Parson name = {naykos[0]}nayka = 'Bobita' > < /Parson> 
      </div>
   )
 }
 
-function Parson() {
+function Parson(props) {
   const ParsonStyle = {
-    border:'2px solid red',
-    margin:'10px',
+    border: '2px solid red',
+    margin: '10px',
   }
-  return ( < div style = {
-        ParsonStyle
-      } >
-    <h1 > Hello!I Am Prince </h1> 
-    <h4 > I Am a student < /h4> 
+  return ( < div style = { ParsonStyle} >
+    <h1 > Nayok : {props.name} < /h1>  
+    < h4 > Here of: {props.nayka} < /h4>  
     </div>)
   }
-  function Parson1() {
-    return ( < div >
-      <
-      h1 > Hello!I Am Abir < /h1>  <
-      h4 > I Am a Doctor < /h4>  <
-      /div>)
-    }
+
 
 
   export default App;
