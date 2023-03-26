c47ecf7f3f4ffc890869f8478eb95417e0e3ef7e
prince954 <akprince0268@gmail.com>
2023-03-27 02:03:54 +0600

    Style:css add

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name ={
+    name:"Prince",
+    job : "Web Developer",
+  }
+  var headerStyle = {
+    color:'red',
+    background:'yellow',
+    padding:'10px 30px'
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p> 
+        < h1 className = "" style = { headerStyle} > Hello!I am {name.name} and my age is { name.job } < /h1>
+        
       </header>
     </div>
   );
