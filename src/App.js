b5e2039088f3b1595b92b8608e9406b52e00a6fd
prince954 <akprince0268@gmail.com>
2023-03-28 01:38:06 +0600

    React :Object Destructuring

--- src/App.js
+++ src/App.js
@@ -1,32 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const naykos = ["Anawr", "Salmal Sha","alomgir"]
+  const naykos = ["Anawr", "Salmal Sha","alomgir"];
+  const products = [{Name:'Photoshop',Price:'50.99'},
+                    {Name : "Illasteter",Price:'45.87'}
+                  ]
 
   return ( < div className = 'App' >
-    < header className = 'app-header' >
-       < p > I am a react Parson < /p> 
+    <header header className = 'app-header'>
+       <p> I am a react Parson </p> 
      </header >
-     <Parson name='Rubel Kahan' nayka ='Moushome'> < /Parson>
-     < Parson name = {naykos[1]} nayka = 'shabana' > < /Parson> 
-     < Parson name = {naykos[2]}nayka = 'popi' > < /Parson> 
-     < Parson name = {naykos[0]}nayka = 'Bobita' > < /Parson> 
+
+
+        <Parson name = 'Rubel Kahan' nayka ='Moushome'></Parson>
+        <Parson name = {naykos[1]}  nayka = 'shabana'></Parson> 
+        <Parson name = {naykos[2]}  nayka = 'popi'></Parson> 
+        <Parson name = {naykos[0]}  nayka = 'Bobita'></Parson> 
+        {/* ----------Array call----------------- */}
+        <Product productName = {products[0]} ProductPrice ={products[0].Price}></Product>
+        {/* ---------------------------- */}
+
+        <Product productName = {products[1]} ProductPrice={products[1].Price}></Product>
      </div>
   )
 }
+
+// first function 
 
 function Parson(props) {
   const ParsonStyle = {
     border: '2px solid red',
     margin: '10px',
+    
   }
-  return ( < div style = { ParsonStyle} >
-    <h1 > Nayok : {props.name} < /h1>  
-    < h4 > Here of: {props.nayka} < /h4>  
-    </div>)
+
+  return ( <div style = {ParsonStyle}>
+    <h1> Nayok : {props.name} </h1>  
+    <h4> Here of: {props.nayka} </h4>  
+    </div>
+    )
   }
+
+// secend function ---------------
+
+  function Product(props){
+    const productStyle = {
+      border:'1px solid gray',
+      borderRadius:'5px',
+      height:'200px',
+      width:'200px',
+      margin:'5px 5%',
+      background:'gray',
+    }
+    const Buttonstyle = {
+      border:'1px solid red',
+      padding:'5px 8px',
+      borderRadius:'5px',
+    }
+    //obj Destructuring  
+    const {Name,Price} =props.productName;
+    // const {Name,Price} =props.ProductPrice;
+
+    return(
+       <div style={productStyle}>
+          {/* <h3>Name : {props.productName.Name} </h3> */}
+          <h3>Name : {Name} </h3>
+          <h1>$ {Price}</h1>
+          <button style={Buttonstyle}>Click Me</button>
+       </div>
+    )
+  }
+
+
+
+
+
 
 
 
